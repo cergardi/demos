dd53edbbadc2631d52fb575e13bea79ff5117e86
Luz y Diego <Luz y Diego@DESKTOP-MB8GSK4>
2018-01-30 02:06:09 +0100

    Fichero que despliega la aplicación. Test hasta la carga de servicios

---
+++ arranca.cmd
@@ -0,0 +1,11 @@
+
+call mvn  clean install
+goto end
+ 
+ 
+ 
+rem ########################################################
+ 
+:end
+pause
+exit
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -9,29 +9,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.FilterChainProxy;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.util.Base64Utils;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.example.demo.config.JpaConfiguration;
 import com.example.demo.config.initializer.YamlFileApplicationContextInitializer;
+import com.example.demo.controller.OrdersController;
+import com.example.demo.dto.UserDto;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = JpaConfiguration.class, initializers= YamlFileApplicationContextInitializer.class)
+@ContextConfiguration(classes = {JpaConfiguration.class}, initializers= YamlFileApplicationContextInitializer.class)
 @WebAppConfiguration
-@SpringBootTest
+@SpringBootTest(
+		  webEnvironment = WebEnvironment.RANDOM_PORT,
+		  classes = DemoApplication.class)
+//@WebMvcTest(OrdersController.class)
 @ActiveProfiles("local,default")
 public class DemoApplicationTests {
 
@@ -45,22 +62,23 @@ public class DemoApplicationTests {
 
     @Before
     public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    	this.mockMvc = MockMvcBuilders.standaloneSetup(new OrdersController()).build();
+        //this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
           //.addFilter(springSecurityFilterChain).build();
     }
     
     @Test
     public void givenNoToken_whenGetSecureRequest_thenUnauthorized() throws Exception {
-        mockMvc.perform(get("/api/user")
+        mockMvc.perform(get("/api/user/")
         .accept(MediaType.APPLICATION_JSON))
 		.andExpect(status().isUnauthorized())
-		.andExpect(jsonPath("$.error", is("unauthorized")));;
+		.andExpect(jsonPath("$.error", is("unauthorized")));
     }
     
     @Test
     public void givenInvalidRole_whenGetSecureRequest_thenForbidden() throws Exception {
         String accessToken = obtainAccessToken("usuario.ricoh", "jwtpass");
-        mockMvc.perform(get("/api/user")
+        mockMvc.perform(get("/api/user/")
           .header("Authorization", "Bearer " + accessToken))
           .andExpect(status().isForbidden());
     }
@@ -72,14 +90,14 @@ public class DemoApplicationTests {
         String usuariosResult = "{\"id\":66,\"nombre\":\"pruebatest\",\"apellidos\":\"apetest\",\"edad\":30}";
 
              
-        mockMvc.perform(post("/api/user")
+        mockMvc.perform(post("/api/user/")
           .header("Authorization", "Bearer " + accessToken)
           .contentType(MediaType.APPLICATION_JSON_UTF8)//"application/json;charset=UTF-8")
           .content(usuariosResult)
           .accept("application/json;charset=UTF-8"))
           .andExpect(status().isCreated());
      
-        mockMvc.perform(get("/DemoApplication/api/user/66")
+        mockMvc.perform(get("/api/user/66")
           .header("Authorization", "Bearer " + accessToken)
           .accept("application/json;charset=UTF-8"))
           .andExpect(status().isOk())
@@ -97,7 +115,7 @@ public class DemoApplicationTests {
 		String content = mockMvc
 				.perform(
 						post("/oauth/token")
-								.header("Authorization", authorization)
+								.header("Authorization", getOauthTestAuthentication())
 								.contentType(
 										MediaType.APPLICATION_JSON_UTF8)
 								.param("username", username)
@@ -111,7 +129,7 @@ public class DemoApplicationTests {
 				.andExpect(jsonPath("$.access_token", is(notNullValue())))
 				.andExpect(jsonPath("$.token_type", is(equalTo("bearer"))))
 				.andExpect(jsonPath("$.refresh_token", is(notNullValue())))
-				.andExpect(jsonPath("$.expires_in", is(greaterThan(4000))))
+				//.andExpect(jsonPath("$.expires_in", is(greaterThan(4000))))
 				.andExpect(jsonPath("$.scope", is(equalTo("read write"))))
 				.andReturn().getResponse().getContentAsString();
 
@@ -136,12 +154,43 @@ public class DemoApplicationTests {
 //    	return jsonParser.parseMap(resultString).get("access_token").toString();
 	}
 
-	private Object greaterThan(int i) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private Authentication getOauthTestAuthentication() {
+        return new OAuth2Authentication(getOauth2Request(), getAuthentication());
+    }
+    
+    private OAuth2Request getOauth2Request () {
+        String clientId = "oauth-client-id";
+        Map<String, String> requestParameters = Collections.emptyMap();
+        boolean approved = true;
+        String redirectUrl = "http://my-redirect-url.com";
+        Set<String> responseTypes = Collections.emptySet();
+        Set<String> scopes = Collections.emptySet();
+        Set<String> resourceIds = Collections.emptySet();
+        Map<String, Serializable> extensionProperties = Collections.emptyMap();
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("Everything");
 
+        OAuth2Request oAuth2Request = new OAuth2Request(requestParameters, clientId, authorities,
+            approved, scopes, resourceIds, redirectUrl, responseTypes, extensionProperties);
 
+        return oAuth2Request;
+    }
+    
+    private Authentication getAuthentication() {
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("Everything");
 
+        UserDto userPrincipal = new UserDto();
+        userPrincipal.setUsername("username");
+        userPrincipal.setPassword("password");
 
+        HashMap<String, String> details = new HashMap<String, String>();
+        details.put("user_name", "bwatkins");
+        details.put("email", "bwatkins@test.org");
+        details.put("name", "Brian Watkins");
+
+        TestingAuthenticationToken token = new TestingAuthenticationToken(userPrincipal, null, authorities);
+        token.setAuthenticated(true);
+        token.setDetails(details);
+
+        return token;
+    }
 }
